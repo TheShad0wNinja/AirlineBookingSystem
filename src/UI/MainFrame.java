8857449e498f07de6eef3da6184ec32e8393872c
TheShad0wNinja <blacklistmillion@gmail.com>
2024-04-27 13:32:18 +0300

    Set main frame

--- src/UI/MainFrame.java
+++ src/UI/MainFrame.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 
 public class MainFrame extends JFrame {
     public MainFrame() {
+        this.setSize(500, 500);
         this.setVisible(true);
         this.pack();
     }
